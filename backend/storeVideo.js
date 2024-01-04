4c623cacb28b8d4f0c2037ae8bf01a6a1b68d28f
Henry <henry.a.s.arrovin@gmail.com>
2024-01-04 20:52:31 +0530

    Added storeVideo.js

---
+++ backend/storeVideo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const fs = require('fs').promises;
+
+async function run() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/action-character', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const videoSchema = new mongoose.Schema({
+      name: String,
+      video: Buffer,
+    });
+
+    const VideoModel = mongoose.model('Video', videoSchema);
+
+    const videoName = 'Demo';
+    const videoBuffer = await fs.readFile('C:/Users/Henry/Desktop/action-character/backend/0001-0050.mkv');
+
+    const newVideo = new VideoModel({ name: videoName, video: videoBuffer });
+
+    await newVideo.save();
+
+    console.log('Video stored in MongoDB');
+
+    await mongoose.connection.close();
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+}
+
+run();
